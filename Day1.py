be70b52cbe3e021e14c8d703b63b7c9fa72cb5be
Stefan Hettgen <hettgen@sheridancollege.ca>
2024-12-17 03:02:14 -0500

    Solved First Part

--- Day1.py
+++ Day1.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from collections import defaultdict
 
 def getRawHtml(pageUrl, sessionCookie):
 
@@ -14,23 +15,21 @@ def getRawHtml(pageUrl, sessionCookie):
 
 
 ## items is a 2d array
-def fillList(items, text):
-
-    x = 0
+def fillList(Arr1, Arr2, text):
     line = 0
     
     while(len(text) > 0):
 
         index1 = text.find(' ')
         index2 = text.find('\n')
-        items[line][0] = int(text[:index1])
-        items[line][1] = int(text[index1+1:index2])
+        Arr1[line] = int(text[:index1])
+        Arr2[line] = int(text[index1+1:index2])
         text = text[index2+1:]
         line += 1
 
 
         
-def findDifference():
+# def findDifference():
 
 
 
@@ -39,15 +38,28 @@ def findDifference():
 def main():
 
     pageUrl = "https://adventofcode.com/2024/day/1/input"
-    sessionCookie = "53616c7465645f5fcdde4f3fd0b01830e3f05c8208a401c2f30aec774c7717cd1bdef9938620f66cc5bca2e04711d0325fdae3f47e7fb0e94bed8926d9ccc01b"
+    sessionCookie = "53616c7465645f5f4bbbbf992fb3a1b72dc263e30d9abbe167a35eb6c1f08188cc90b459bdf6e543ad0204885f37b4c0e8a4e8028fc3431453bcf646e1539e17"
     text = getRawHtml(pageUrl, sessionCookie)
 
-    print(type(text))
+    lines = text.count('\n')
+    Arr1 = [0] * lines
+    Arr2 = [0] * lines
 
-    textList = [[None for _ in range(2)] for _ in range(text.count('\n'))]
 
-    fillList(textList, text)
-    print(textList)
+    fillList(Arr1, Arr2, text)
+
+    valueCounts = defaultdict(int)
+
+    Arr1.sort()
+    Arr2.sort()
+
+    totalVal = 0
+
+    for x in range(len(Arr1)):
+        totalVal += abs(Arr1[x] - Arr2[x])
+        
+    
+    print(totalVal)
 
 
 
